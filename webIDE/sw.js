dc83f82e797adc5ad71f6f2a49715a92aca2da67
Gilad Bracha <gilad@bracha.org>
2024-07-09 11:50:13 -0700

    Fix issue 70
    Change how SystemScope>allSelectorsDo: works

--- webIDE/sw.js
+++ webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 131;
+const pwaVersion = 132;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;
