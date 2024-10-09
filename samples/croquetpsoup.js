9e8858f890b1b73b857a64a5c990a798865cc748
Gilad Bracha <gilad@bracha.org>
2024-10-09 15:57:42 -0700

    Update croquetpsoup.js

--- samples/croquetpsoup.js
+++ samples/croquetpsoup.js
@@ -3474,7 +3474,8 @@ function nsCodeMirrorChange(change) {
     console.log('from ch =' + change.from.ch);
     console.log('from line =' + change.from.line);
     console.log('to ch =' + change.to.ch);
-    console.log('to line =' + change.to.line);    
+    console.log('to line =' + change.to.line);
+    console.log('textlength'+change.text.length);
     return {from: nsCursorPos(change.from.ch, change.from.line),
 	    to:  nsCursorPos(change.to.ch, change.to.line),
 	    text: change.text,
