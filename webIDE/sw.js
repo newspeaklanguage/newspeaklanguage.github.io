c4ebd40cc8c17af73027a5a3efe09c4dd3059f56
Gilad Bracha <gilad@bracha.org>
2026-05-14 19:58:57 -0700

    AI updates: evaluation
    AI support for evaluattion

--- webIDE/sw.js
+++ webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 860;
+const pwaVersion = 861;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;
