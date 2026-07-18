7b5b2f7e53f503690b86f5c84221db47adb26952
Gilad Bracha <gilad@bracha.org>
2026-07-18 14:33:22 -0700

    Latest version. Document and JS fixes.

--- webIDE/sw.js
+++ webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 900;
+const pwaVersion = 901;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;
